fd2f214d800a9ec3500a671c5748b47de66cdb58
Damiano <mura.Damiano14@gmail.com>
2023-11-06 23:11:50 +0000

    improved seeder with 3 users per role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         //dev User creation
         \App\Models\User::factory()->create([
             'name' => 'Dev',
-            'email' => 'admin@rpg.game',
+            'email' => 'dev@rpg.game',
             'password' => Bcrypt('administrator'),
             'current_team_id' => 1,
         ]);
@@ -33,18 +33,49 @@ class DatabaseSeeder extends Seeder
             'display_name' => 'Developer',
             'description' => 'Developer',
         ]);
+        //admin User creeation
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@rpg.game',
+            'password' => Bcrypt('administrator'),
+            'current_team_id' => 2,
+        ]);
+        //admin Team creation
+        \App\Models\Team::factory()->create([
+            'name' => 'Admin',
+            'user_id' => 2,
+            'personal_team' => true,
+        ]);
+        //admin Role creation
         \App\Models\Role::factory()->create([
             'name' => 'Admin',
             'display_name' => 'Administrator',
             'description' => 'Administrator',
         ]);
+        //user User creation
+        \App\Models\User::factory()->create([
+            'name' => 'Dev',
+            'email' => 'admin@rpg.game',
+            'password' => Bcrypt('administrator'),
+            'current_team_id' => 1,
+        ]);
+        //user Team creation
+        \App\Models\Team::factory()->create([
+            'name' => 'Developer',
+            'user_id' => 1,
+            'personal_team' => true,
+        ]);
+        //user Role creation
         $role=\App\Models\Role::create([
             'name' => 'User',
             'display_name' => 'User',
             'description' => 'User',
         ]);
-        $user=\App\Models\User::find(1);
-        $user->attachRole(1);
+        for($x=0;$x<3;$x++){
+            $user=\App\Models\User::find($x+1);
+            $user->attachRole($x+1);
+        }
+        
         
     }
 }
